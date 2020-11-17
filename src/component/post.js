698b3f3469cfa19aff07a2fed490984a1dc201dd
Purvi jha <51432532+Purvicapternal@users.noreply.github.com>
2020-11-16 21:38:10 -0800

    Add files via upload

---
+++ src/component/post.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Posts() {
+    return (
+        <div style={{ display: "flex" }}>
+             <h1>hiii from post</h1>
+        </div>
+    )
+}
